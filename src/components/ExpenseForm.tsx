0b8b66fc3a3635d69ca65bd4afc9974ba2d9b0c4
Divya Shree <shree.d@northeastern.edu>
2024-09-06 23:12:05 -0400

    Added env variables

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -13,6 +13,7 @@ const ExpenseForm: React.FC = () => {
   const [amount, setAmount] = useState("");
   const [date, setDate] = useState("");
   const [expenses, setExpenses] = useState<Expense[]>([]);
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   // Fetch all expenses from the backend when the component mounts
   useEffect(() => {
@@ -21,7 +22,7 @@ const ExpenseForm: React.FC = () => {
 
   const fetchExpenses = async () => {
     try {
-      const response = await axios.get("http://localhost:8080/expense");
+      const response = await axios.get(`${apiUrl}/expense`);
       setExpenses(response.data);
     } catch (error) {
       console.error("Error fetching expenses:", error);
@@ -47,7 +48,7 @@ const ExpenseForm: React.FC = () => {
 
     try {
       // Send POST request to backend to save the expense
-      await axios.post("http://localhost:8080/expense", newExpense);
+      await axios.post(`${apiUrl}/expense`, newExpense);
       alert("Expense saved!");
 
       // Clear the form inputs after submission
